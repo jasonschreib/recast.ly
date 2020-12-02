5a30b42e12c818db70bfbbc97d196746e57fb5f3
jasonschreib <jwschreiber1@gmail.com>
2020-12-02 13:55:41 -0800

    Complete creating a dynamic Video List component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+import VideoListEntry from '../../src/components/VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(eachVideo => <VideoListEntry video={eachVideo} />)}
   </div>
 );
 
@@ -17,3 +15,15 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
+/*
+update app (final component) with a videoList of videoListEntries
+*/
+
+
+/* <div><h5><em>{props.videos[0]}</em> view goes here</h5></div>
+    <div><h5><em>VideoListEntry</em> view goes here</h5></div>
+    <div><h5><em>VideoListEntry</em> view goes here</h5></div>
+    <div><h5><em>VideoListEntry</em> view goes here</h5></div>
+    <div><h5><em>VideoListEntry</em> view goes here</h5></div> */
